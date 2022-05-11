b3de0dfb620a4d5e2fd4b70aa6e7d64e52c55f52
MurukLuan <murukluan@gmail.com>
2022-05-11 09:16:49 -0300

    exercicio da aula de terça-feira sobre listas

---
+++ ExercicioListasEmSala-10-05-2022/ExercicioListasEmSala-10-05-2022/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioListasEmSala_10_05_2022
+{
+    class Funcionario 
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public int Idade { get; set; } 
+        public string Email { get; set; }
+        public string Departamento { get; set; }
+
+        public Funcionario(string nome, double salario, int idade, string email, string departamento)
+        {
+            Nome = nome;
+            Salario = salario;
+            Idade = idade;
+            Email = email;
+            Departamento = departamento;
+        } 
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome
+                + "\nIdade: " + Idade
+                + "\nEmai: " + Email
+                + "\nDepartamento: " + Departamento
+                + "\nSalario: " + Salario;
+        }
+    }
+}
---
+++ ExercicioListasEmSala-10-05-2022/ExercicioListasEmSala-10-05-2022/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioListasEmSala_10_05_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcao;
+            
+            List<Funcionario> func = new List<Funcionario>();
+            do
+            {
+                Console.Write("Entre com o nome do funcionario: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Entre com o salario do funcionario: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                Console.Write("Entre com a idade do funcionario: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.Write("Entre com o e-mail do funcionario: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Entre com o departamento do funcionario: ");
+                string departamento = Console.ReadLine();
+                departamento.ToLower();
+
+                func.Add(new Funcionario(nome, salario, idade, email, departamento));
+
+                Console.Write("Deseja cadastrar mais um funcionario? (s/n)");
+                opcao = char.Parse(Console.ReadLine());
+                
+
+            } while (opcao == 's');
+
+            Console.WriteLine("-----------------------------------------------");
+
+            foreach (Funcionario obj in func)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Quantidade de trabalhadores: " + func.Count);
+
+            int contador = 0;
+            if (func.Exists(x => x.Departamento == "rh"))
+            {
+                foreach(Funcionario obj in func)
+                {                    
+                    contador++;
+                }
+                Console.WriteLine(contador);
+            }
+            
+
+
+
+
+        }
+    }
+}
